e2e94641ff8d2306482f3a751cd538df4d00a5bf
James Husband <jchusband@proton.me>
2024-12-22 14:28:49 +0000

    add ui-components lib

---
+++ libs/ui/components/README.md
@@ -0,0 +1,7 @@
+# UI-Shared
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test UI-Shared` to execute the unit tests via [Jest](https://jestjs.io).
---
+++ libs/ui/components/src/lib/UI-Shared.tsx
@@ -0,0 +1,11 @@
+import styles from './UI-Shared.module.css';
+
+export function UIShared() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to UIShared!</h1>
+    </div>
+  );
+}
+
+export default UIShared;
